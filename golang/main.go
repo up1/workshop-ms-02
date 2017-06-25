c8f89017bb68b078293e505930f302bd4e6e17ef
Santi Lertsumran <santi@inslash.com>
2017-06-25 15:44:02 +0700

    add mocking data in feed

--- golang/main.go
+++ golang/main.go
@@ -2,21 +2,53 @@ package main
 
 import "gopkg.in/gin-gonic/gin.v1"
 
-// You also can use a struct
-var feed struct {
+type like struct {
+	Count int      `json:"count"`
+	Name  []string `json:"name"`
+}
+
+type data struct {
 	ID          int    `json:"id"`
 	Description string `json:"description"`
-	Like        struct {
-		Count int      `json:"count"`
-		Name  []string `json:"name"`
-	} `json:"like"`
+	Like        like   `json:"like"`
+}
+
+var feed []data
+
+func init() {
+	feed = []data{
+		data{
+			ID:          1,
+			Description: "test1",
+			Like: like{
+				Count: 1,
+				Name:  []string{"first name", "peach", "pear"},
+			},
+		},
+		data{
+			ID:          2,
+			Description: "test2",
+			Like: like{
+				Count: 1,
+				Name:  []string{"second name", "hello", "somkait"},
+			},
+		},
+		data{
+			ID:          3,
+			Description: "test3",
+			Like: like{
+				Count: 1,
+				Name:  []string{"third name", "world", "pear"},
+			},
+		},
+	}
 }
 
 func main() {
-	feed.ID = 1
-	feed.Description = "test"
-	feed.Like.Count = 1
-	feed.Like.Name = []string{"first name", "peach", "pear"}
+	feed[0].ID = 1
+	// feed.Data[0].Description = "test"
+	// feed.Data[0].Like.Count = 1
+	// feed.Data[0].Like.Name = []string{"first name", "peach", "pear"}
 	r := gin.Default()
 	r.GET("/feed", func(c *gin.Context) {
 		c.JSON(200, feed)
