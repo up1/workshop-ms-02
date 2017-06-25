33eb491b5c6683ef9a3d9943b2ac10a6937000c5
sorrapong <sorrapong.w@gmail.com>
2017-06-25 16:25:04 +0700

    java call service Profile

--- java/service.profile/src/main/java/service/profile/controller/ProfileController.java
+++ java/service.profile/src/main/java/service/profile/controller/ProfileController.java
@@ -3,16 +3,22 @@ package service.profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import service.profile.entity.Profile;
+import service.profile.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class ProfileController{
 
+    @Autowired
+    ProfileService profileService;
+
     @GetMapping("/profile")
-    public String getProfile(@RequestParam(value="ID", defaultValue="") String ID){
+    public Profile getProfile(@RequestParam(value="ID", defaultValue="") String ID){
 
-        if (ID.equals("")) return "service require ID";
+        if (ID.equals("")) return new Profile();       
 
-        return ID;
+        Profile profile = profileService.getProfile(1);
+        return profile;
     }
-
 }
