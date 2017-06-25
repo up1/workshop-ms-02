1eee73f04a82dc90df6aee9f3b79a409fe3577df
Santi Lertsumran <santi@inslash.com>
2017-06-25 15:24:32 +0700

    add struct feed

--- golang/main.go
+++ golang/main.go
@@ -2,14 +2,24 @@ package main
 
 import "gopkg.in/gin-gonic/gin.v1"
 
+// You also can use a struct
+var feed struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+	Like        struct {
+		Count int      `json:"count"`
+		Name  []string `json:"name"`
+	} `json:"like"`
+}
+
 func main() {
+	feed.ID = 1
+	feed.Description = "test"
+	feed.Like.Count = 1
+	feed.Like.Name = []string{"first name", "peach", "pear"}
 	r := gin.Default()
 	r.GET("/feed", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"id":          1,
-			"description": "test",
-			"like":        1,
-		})
+		c.JSON(200, feed)
 	})
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
