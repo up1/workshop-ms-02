ffa4e03c004001022d734c59322c17ce6e949575
Santi Lertsumran <santi@inslash.com>
2017-06-25 16:09:14 +0700

    move to directory

---
+++ golang/feed_service/feed_service.go
@@ -0,0 +1,53 @@
+package main
+
+import "gopkg.in/gin-gonic/gin.v1"
+
+type like struct {
+	Count int      `json:"count"`
+	Name  []string `json:"name"`
+}
+
+type data struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+	Like        like   `json:"like"`
+}
+
+var feed []data
+
+func init() {
+	feed = []data{
+		data{
+			ID:          1,
+			Description: "test1",
+			Like: like{
+				Count: 1,
+				Name:  []string{"first name", "peach", "pear"},
+			},
+		},
+		data{
+			ID:          2,
+			Description: "test2",
+			Like: like{
+				Count: 1,
+				Name:  []string{"second name", "hello", "somkait"},
+			},
+		},
+		data{
+			ID:          3,
+			Description: "test3",
+			Like: like{
+				Count: 1,
+				Name:  []string{"third name", "world", "pear"},
+			},
+		},
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/feed", func(c *gin.Context) {
+		c.JSON(200, feed)
+	})
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080
+}
