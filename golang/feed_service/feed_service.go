425eae006fc1d97c48463198e39331ef1e33ef45
Santi Lertsumran <santi@inslash.com>
2017-06-26 18:27:29 +0700

    change mockData to real JSON format

--- golang/feed_service/feed_service.go
+++ golang/feed_service/feed_service.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/gin-gonic/gin"
 	"github.com/mcuadros/go-gin-prometheus"
 )
@@ -17,34 +19,47 @@ type data struct {
 }
 
 var feed []data
+var mockData []byte
 
 func init() {
-	feed = []data{
-		data{
-			ID:          1,
-			Description: "test1",
-			Like: like{
-				Count: 1,
-				Name:  []string{"first name", "peach", "pear"},
-			},
-		},
-		data{
-			ID:          2,
-			Description: "test2",
-			Like: like{
-				Count: 1,
-				Name:  []string{"second name", "hello", "somkait"},
-			},
-		},
-		data{
-			ID:          3,
-			Description: "test3",
-			Like: like{
-				Count: 1,
-				Name:  []string{"third name", "world", "pear"},
-			},
-		},
+	mockData = []byte(`[
+	{
+		"id": 1,
+		"description": "test1",
+		"like": {
+			"count": 1,
+			"name": [
+				"first name",
+				"peach",
+				"pear"
+			]
+		}
+	},
+	{
+		"id": 2,
+		"description": "test2",
+		"like": {
+			"count": 1,
+			"name": [
+				"second name",
+				"hello",
+				"somkait"
+			]
+		}
+	},
+	{
+		"id": 3,
+		"description": "test3",
+		"like": {
+			"count": 1,
+			"name": [
+				"third name",
+				"world",
+				"pear"
+			]
+		}
 	}
+]`)
 }
 
 func main() {
@@ -53,6 +68,10 @@ func main() {
 	p := ginprometheus.NewPrometheus("gin")
 	p.Use(r)
 	r.GET("/feed", func(c *gin.Context) {
+		err := json.Unmarshal(mockData, &feed)
+		if err != nil {
+			panic(err)
+		}
 		c.JSON(200, feed)
 	})
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080
